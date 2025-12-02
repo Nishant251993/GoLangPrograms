696526f3bf39f4fb02fbe2edaa90869c88ec7dd0
Nishant patel <patel.nishant21@gmail.com>
2025-12-02 21:15:12 +0530

    short variable declaration and different datatypes

--- shorthandwayVariable.go
+++ shorthandwayVariable.go
@@ -3,7 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var s, t string = "Nishant", "Patel"
+
+	//for same data type
+	var firstname, lastanme string = "Nishant", "Patel"
+	fmt.Println(firstname)
+	fmt.Println(lastanme)
+
+	//for different data type
+	var (
+		s string = "Nishant"
+		t int    = 50000
+	)
+
 	fmt.Println(s)
 	fmt.Println(t)
+
+	//short variable declaration
+	shortv := "Short variable declaration"
+
+	fmt.Println(shortv)
 }
